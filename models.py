00f4a195efb473bf2b3c616f78569c42ac2a858a
bettayub <bettayub19@gmail.com>
2023-09-08 16:59:10 +0300

    Creating the SQLAlchemy base and engine for the SQLite database

--- models.py
+++ models.py
@@ -1,5 +1,12 @@
+
 # Import necessary modules and create the SQLAlchemy engine and base.
+
 # models.py
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+
+# Create the SQLAlchemy base and engine for the SQLite database.
+Base = declarative_base()
+engine = create_engine('sqlite:///school.db')
+
