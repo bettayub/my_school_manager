4f20026a048f56847676583b757c3a9427fc2c10
bettayub <bettayub19@gmail.com>
2023-09-08 17:16:13 +0300

    Function to add academic marks for a student (Teacher)

--- main.py
+++ main.py
@@ -21,3 +21,21 @@ def register_student():
     session.commit()
     print(f"Registered student: {name}, Admission Number: {adm}")
 
+
+# Function to add academic marks for a student (Teacher)
+def add_academics():
+    print("Add Academics")
+    student_id = int(input("Enter student ID: "))
+    student = session.query(Student).filter_by(student_id=student_id).first()
+    
+    if not student:
+        print("Student not found.")
+        return
+
+    marks = int(input("Enter academic marks: "))
+    # Update the student's academic marks and commit the changes
+    student.student_academics = marks
+    session.commit()
+    print(f"Academic marks added for {student.student_name}: {marks}")
+
+
