fffe29f9ac16664d6d150847ac37428365eb2f0f
bettayub <bettayub19@gmail.com>
2023-09-08 17:00:26 +0300

    Defining the Student class for the 'students' table

--- models.py
+++ models.py
@@ -10,3 +10,26 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 engine = create_engine('sqlite:///school.db')
 
+
+# Define the Student class for the 'students' table.
+class Student(Base):
+    __tablename__ = 'students'
+
+    # Define columns for student information.
+    student_id = Column(Integer, primary_key=True)
+    student_name = Column(String)
+    student_adm = Column(String)
+    student_academics = Column(Integer)
+    student_attendance = Column(Float)
+    
+    # Add the student_fee_balance column.
+    student_fee_balance = Column(Integer)
+
+    # Define relationships with other tables.
+    parent_id = Column(Integer, ForeignKey('parents.parent_id'))
+    teacher_id = Column(Integer, ForeignKey('teachers.teacher_id'))
+    parent = relationship('Parent', back_populates='students')
+    teacher = relationship('Teacher', back_populates='students')
+    fees = relationship('Fee', back_populates='student')
+    enrollments = relationship('Enrollment', back_populates='student')
+    courses = relationship('Course', secondary='enrollments', back_populates='students')
