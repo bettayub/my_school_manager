25850a6b88462e2f306dcb9c41acafd002cfaf23
bettayub <bettayub19@gmail.com>
2023-09-08 15:14:52 +0300

    Adding python files

---
+++ models.py
@@ -0,0 +1,5 @@
+# models.py
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
