f53f428ef5468df1e152d00faff23a24bf6fd4b1
bettayub <bettayub19@gmail.com>
2023-09-08 17:12:51 +0300

    Function to register a new student

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ Base.metadata.create_all(engine)
 # Create a session to interact with the database
 Session = sessionmaker(bind=engine)
 session = Session()
+
+
+# Function to register a new student
+def register_student():
+    print("Student Registration")
+    name = input("Enter student's name: ")
+    adm = input("Enter admission number: ")
+    # Create a new student instance and add it to the session
+    student = Student(student_name=name, student_adm=adm, student_fee_balance=0)
+    session.add(student)
+    session.commit()
+    print(f"Registered student: {name}, Admission Number: {adm}")
+
