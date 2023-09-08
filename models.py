7d5e52e51f84a53b9afb72c9d15f9e5aa707c09e
bettayub <bettayub19@gmail.com>
2023-09-08 15:19:26 +0300

    Importing  necessary modules and create the SQLAlchemy engine and base.

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
+# Import necessary modules and create the SQLAlchemy engine and base.
 # models.py
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-
